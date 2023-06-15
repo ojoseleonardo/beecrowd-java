22ae1ecb137d0f55c0b83f199bb53f7e2784c642
José Leonardo <joseleonardosantoos@outlook.com>
2023-06-15 16:03:17 -0300

    Distance Between Two Points #1015

---
+++ distanceBetweenTwoPoints/DistanceBetweenTwoPoints.java
@@ -0,0 +1,14 @@
+package beginner;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class DistanceBetweenTwoPoints {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
